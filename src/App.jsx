081d91b5a34a03ed4fc43c7a780e9fa022e01074
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-07-21 15:09:49 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => console.log('click in')}>Check in</Button>
+        <Button onClick={() => console.log('click out')}>Check out</Button>
+
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
